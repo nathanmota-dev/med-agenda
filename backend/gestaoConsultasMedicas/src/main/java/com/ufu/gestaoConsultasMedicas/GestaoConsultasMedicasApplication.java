85a19cc833afd3f1b3084f62ba2f4e8ee1a48bf7
Nathan <nathansmota@gmail.com>
2024-10-19 14:36:58 -0300

    :tada: initial commit

---
+++ backend/gestaoConsultasMedicas/src/main/java/com/ufu/gestaoConsultasMedicas/GestaoConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.ufu.gestaoConsultasMedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ backend/gestaoConsultasMedicas/src/test/java/com/ufu/gestaoConsultasMedicas/GestaoConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufu.gestaoConsultasMedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
