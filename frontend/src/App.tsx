d1d6b28eceb340e7ab9ef44552b5e245877d84cc
Nathan <nathansmota@gmail.com>
2024-10-27 19:58:56 -0300

    :sparkles: feat: creating dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard.tsx";
 import Register from "./pages/register/index.tsx";
+import Admin from "./pages/admin/index.tsx";
+import Patients from "./pages/patients/index.tsx";
+import Doctors from "./pages/doctors/index.tsx";
+import Consultations from "./pages/consultations/index.tsx";
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dash" element={<Dashboard />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/patients" element={<Patients />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/consultations" element={<Consultations />} />
     </Routes>
   )
 }
