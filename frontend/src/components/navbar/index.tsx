9283b163328fba6e5cfef4cc47421f43b0145eb3
Lucas Manzato Gonçalves <129764548+lucasmanzato@users.noreply.github.com>
2025-08-27 21:36:25 -0300

    Update index.tsx

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -57,7 +57,7 @@ export default function PublicNavbar() {
                                 className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                                 aria-current="page"
                             >
-                                Médicos
+                                Especialidades Médicas
                             </a>
                         </li>
                     </ul>
